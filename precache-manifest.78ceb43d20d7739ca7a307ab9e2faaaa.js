5cffe8c26075590fc9d2228e314a317cfda0effe
Catalin <lungancatalin@yahoo.com>
2020-04-26 13:24:28 +0300

    Updates

---
+++ precache-manifest.78ceb43d20d7739ca7a307ab9e2faaaa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed18da7394f16824ec060451f199f32b",
+    "url": "/huddle/index.html"
+  },
+  {
+    "revision": "562dfef9350a0fc8a389",
+    "url": "/huddle/static/css/main.4a1c0e02.chunk.css"
+  },
+  {
+    "revision": "eff39fefa1bc21c10b5f",
+    "url": "/huddle/static/js/2.a384006f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/huddle/static/js/2.a384006f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562dfef9350a0fc8a389",
+    "url": "/huddle/static/js/main.d6952613.chunk.js"
+  },
+  {
+    "revision": "84e1f7c0b37780f4ac4b",
+    "url": "/huddle/static/js/runtime-main.8bf4ac3a.js"
+  },
+  {
+    "revision": "6641e825f006d463db7028607e3ee994",
+    "url": "/huddle/static/media/illustration-flowing-conversation.6641e825.svg"
+  },
+  {
+    "revision": "bb27da81d1984faa5ef57668534ff1a5",
+    "url": "/huddle/static/media/illustration-grow-together.bb27da81.svg"
+  },
+  {
+    "revision": "88090acec5276438b135a468364edf12",
+    "url": "/huddle/static/media/illustration-mockups.88090ace.svg"
+  },
+  {
+    "revision": "8a5af9952fb5ed23779d589621b108a5",
+    "url": "/huddle/static/media/illustration-your-users.8a5af995.svg"
+  },
+  {
+    "revision": "d4d1854c4b721cdd4fc94a21f6a8c1a8",
+    "url": "/huddle/static/media/logo-white.d4d1854c.svg"
+  },
+  {
+    "revision": "3cb867305e954abaa1a8b0dd7a4e7c06",
+    "url": "/huddle/static/media/logo.3cb86730.svg"
+  }
+]);
